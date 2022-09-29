fd1a9aecb377066e49e683ffcb52e44b0878fae3
zhangbo <zhangbo@authing.cn>
2022-09-29 11:07:57 +0800

    feat:  add Test

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
-import Charts from './components/Charts.vue';
+// import HelloWorld from "./components/HelloWorld.vue";
+// import Charts from "./components/Charts.vue";
+import { ref } from "vue";
+import Test from "./components/Test.vue";
+
+const refA = ref("Hello");
+
+setTimeout(() => {
+  refA.value = "World";
+}, 3000);
 </script>
 
 <template>
+  <div>
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
-    <Charts></Charts>
+    <!-- <Charts></Charts> -->
+    <Test :msg="refA"></Test>
+  </div>
 </template>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    /* text-align: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center;
   color: #2c3e50;
   margin-top: 60px; */
 }
 body,
 p {
-    margin: 0;
+  margin: 0;
 }
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>{{ props.msg }}</h1>
+
+  <p>{{ places }}</p>
+
+  <p>{{ places2 }}</p>
+
+  <!-- <button type="button" @click="count++">count is: {{ count }}</button> -->
+</template>
+
+<script setup lang="ts">
+import { onBeforeMount, reactive, ref } from "vue";
+
+const props = defineProps<{ msg: string }>();
+
+console.log("props", props);
+
+// const count = ref(0);
+
+interface placeType {
+  name: string;
+  a: string;
+  b?: string;
+}
+
+const places = ref<placeType[]>([]);
+const places2 = reactive<placeType[]>([]);
+
+onBeforeMount(() => {
+  places.value = [{ a: "1", b: "2", name: "11" }];
+
+  places2.push({
+    a: "1",
+    b: "2",
+    name: "11",
+  });
+
+  setTimeout(() => {
+    places.value[0].a = "100";
+
+    places2[0].a = "1000";
+  }, 3000);
+});
+
+console.log(
+  "places.value.map((i) => i.name)",
+  places.value.map((i) => i.name)
+);
+</script>
+
+<style scoped></style>
