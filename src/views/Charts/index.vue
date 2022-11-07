aba4b13e2fb868dba6c0d226c335f8d288c652cb
zhangbo <zhangbo@authing.cn>
2022-11-07 11:03:48 +0800

    publish

--- src/views/Charts/index.vue
+++ src/views/Charts/index.vue
@@ -16,11 +16,13 @@
 
       <div v-for="item in data" :key="item.name" class="input-item">
         <input v-model="item.name" />
-        <input v-model="item.value" type="number" pattern="number" />
+        <!-- <input v-model="item.value" type="number" pattern="number" /> -->
+        <input v-model="item.value" type="text" inputmode="decimal" />
 
         <div v-for="j in item.data" :key="j.name">
           <input v-model="j.name" />
-          <input v-model="j.value" type="number" pattern="number" />
+          <!-- <input v-model="j.value" type="number" pattern="number" /> -->
+          <input v-model="j.value" type="text" inputmode="decimal" />
         </div>
         <button @click="handleAddChild(item)">增加</button>
       </div>
