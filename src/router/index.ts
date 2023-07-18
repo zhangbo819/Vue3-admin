7d51781998d004da539e8bb49d6669a1e7bb2343
zhangbo <zhangbo819@163.com>
2023-07-18 20:31:22 +0800

    feat: add test

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createMemoryHistory, createRouter } from "vue-router";
 import Home from "@/views/Home.vue";
 import Charts from "@/views/Charts/index.vue";
 import DatePicker from "@/views/DatePicker/index.vue";
+import AutoSearch from "@/views/AutoSearch/index.vue";
 
 const routes = [
   {
@@ -18,7 +19,12 @@ const routes = [
     path: "/DatePicker",
     name: "DatePicker",
     component: DatePicker,
-  }
+  },
+  {
+    path: "/AutoSearch",
+    name: "AutoSearch",
+    component: AutoSearch,
+  },
 ];
 
 const router = createRouter({
